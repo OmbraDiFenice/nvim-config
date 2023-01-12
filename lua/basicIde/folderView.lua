89172cbdeab0530d132ad8cd358883c9b57be3a1
Stefano <stefano.stoduto@gmail.com>
2023-01-12 22:53:20 +0100

    further configuration for tree view

--- lua/basicIde/folderView.lua
+++ lua/basicIde/folderView.lua
@@ -1,11 +1,13 @@
+local lib = require("nvim-tree.lib")
+local view = require("nvim-tree.view")
+local api = require("nvim-tree.api")
+
 local function collapse_all()
 	require("nvim-tree.actions.tree-modifiers.collapse-all").fn()
 end
 
 local function edit_or_open()
 	-- open as vsplit on current node
-	local lib = require("nvim-tree.lib")
-	local view = require("nvim-tree.view")
 	local action = "edit"
 	local node = lib.get_node_at_cursor()
 
@@ -14,22 +16,18 @@ local function edit_or_open()
 	-- Just copy what's done normally with vsplit
 	if node.link_to and not node.nodes then
 		require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
-		view.close() -- Close the tree if file was opened
 
 	elseif node.nodes ~= nil then
 		lib.expand_or_collapse(node)
 
 	else
 		require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
-		view.close() -- Close the tree if file was opened
 	end
 
 end
 
 local function vsplit_preview()
 	-- open as vsplit on current node
-	local lib = require("nvim-tree.lib")
-	local view = require("nvim-tree.view")
 	local action = "vsplit"
 	local node = lib.get_node_at_cursor()
 
@@ -51,6 +49,34 @@ local function vsplit_preview()
 	view.focus()
 end
 
+local git_add = function()
+  local node = lib.get_node_at_cursor()
+
+	if not node then return end
+
+  local gs = node.git_status.file
+
+  -- If the file is untracked, unstaged or partially staged, we stage it
+  if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
+    vim.cmd("silent !git add " .. node.absolute_path)
+
+  -- If the file is staged, we unstage
+  elseif gs == "M " or gs == "A " then
+    vim.cmd("silent !git restore --staged " .. node.absolute_path)
+  end
+
+  lib.refresh_tree()
+end
+
+local key_mapping = {
+	{ key = "l", action = "edit", action_cb = edit_or_open },
+	{ key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
+	{ key = "h", action = "close_node" },
+	{ key = "H", action = "collapse_all", action_cb = collapse_all },
+	{ key = "ga", action = "git_add", action_cb = git_add },
+	{ key = "<leader>f", action = ":NvimTreeFindFile<CR>" },
+}
+
 return {
 	use_deps = function(use)
 		use {
@@ -67,17 +93,14 @@ return {
 		vim.g.loaded_netrwPlugin = 1
 		vim.opt.termguicolors = true
 
-		vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", { silent = true, noremap = true })
+		vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = function() api.tree.open() end })
+		vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeFocus<cr>", { silent = true, noremap = true })
+
 		local config = {
 			view = {
 				mappings = {
 					custom_only = false,
-					list = {
-						{ key = "l", action = "edit", action_cb = edit_or_open },
-						{ key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
-						{ key = "h", action = "close_node" },
-						{ key = "H", action = "collapse_all", action_cb = collapse_all }
-					}
+					list = key_mapping,
 				},
 			},
 			actions = {
@@ -87,6 +110,15 @@ return {
 			},
 			renderer = {
 				group_empty = false,
+				indent_markers = {
+					enable = true,
+				},
+				highlight_git = true,
+				icons = {
+					show = {
+						git = false,
+					},
+				},
 			},
 		}
 
