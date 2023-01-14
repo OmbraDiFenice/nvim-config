98801a6beb0115cac0350423f43b19d214205b09
Stefano <stefano.stoduto@gmail.com>
2023-01-14 09:51:40 +0100

    fix icons not showing in nvim-tree
    it seems that packer considers a dependency optional even
    if it's marked with opt=true only once and later required
    as non-opt

--- lua/basicIde/statusBar.lua
+++ lua/basicIde/statusBar.lua
@@ -2,7 +2,7 @@ return {
 	use_deps = function(use)
 		use {
 			'nvim-lualine/lualine.nvim',
-			requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+			requires = { 'kyazdani42/nvim-web-devicons' }
 		}
 	end,
 
