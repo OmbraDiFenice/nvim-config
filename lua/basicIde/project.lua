0d85cc0c2071c70418e79d07b33dc2018d5c22ac
Stefano <stefano.stoduto@gmail.com>
2023-02-08 22:38:27 +0100

    support custom script in project settings

--- lua/basicIde/project.lua
+++ lua/basicIde/project.lua
@@ -1,4 +1,5 @@
 local UNITTEST_TERM_ID = 10
+local CUSTOM_COMMAND_TERM_ID = 11
 
 local function parseCommand(commandDescriptor)
 	local cmd = ""
@@ -7,9 +8,12 @@ local function parseCommand(commandDescriptor)
 		cmd = cmd .. "[[ -z ${VIRTUAL_ENV:+x} ]] || source " .. commandDescriptor.virtualEnv .. "; "
 	end
 
-	for key, value in pairs(commandDescriptor.env) do
-		cmd = cmd .. key .. "=" .. value .. " "
+	if commandDescriptor.env then
+		for key, value in pairs(commandDescriptor.env) do
+			cmd = cmd .. key .. "=" .. value .. " "
+		end
 	end
+
 	cmd = cmd .. commandDescriptor.cmd
 
 	return cmd
@@ -31,6 +35,22 @@ return {
 						require('toggleterm').exec(cmd, UNITTEST_TERM_ID, 20, opts.workDir)
 					end,
 					{ desc = 'Run unit tests' })
+				end,
+
+				customScript = function(opts)
+					vim.keymap.set('n', '<leader>cs', function ()
+						local i = vim.v.count
+						local commandDescriptor = opts[i+1] -- lua tables indexes are 1-based
+
+						if commandDescriptor == nil then
+							print("no custom script registered at index " .. i)
+							return
+						end
+
+						local cmd = parseCommand(commandDescriptor)
+						require('toggleterm').exec(cmd, CUSTOM_COMMAND_TERM_ID + i, 20, commandDescriptor.workDir)
+					end,
+					{ desc = 'Run custom script' })
 				end,
 			},
 		})
