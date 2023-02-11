87a0a6f8665aec45054cbbe184505eeffc61461b
Stefano <stefano.stoduto@gmail.com>
2023-02-11 11:32:45 +0100

    switch from vgit plugin to lazygit through terminal window
    loosing:
     - git gutter
     - revert specific hunk

--- lua/basicIde/git.lua
+++ lua/basicIde/git.lua
@@ -1,40 +1,27 @@
-local keymaps = {
-	['n <C-k>'] = function() require('vgit').hunk_up() end,
-	['n <C-j>'] = function() require('vgit').hunk_down() end,
-	['n <leader>gs'] = function() require('vgit').buffer_hunk_stage() end,
-	['n <leader>gr'] = function() require('vgit').buffer_hunk_reset() end,
-	['n <leader>gp'] = function() require('vgit').buffer_hunk_preview() end,
-	['n <leader>gb'] = function() require('vgit').buffer_blame_preview() end,
-	['n <leader>gf'] = function() require('vgit').buffer_diff_preview() end,
-	['n <leader>gh'] = function() require('vgit').buffer_history_preview() end,
-	['n <leader>gu'] = function() require('vgit').buffer_reset() end,
-	['n <leader>gg'] = function() require('vgit').buffer_gutter_blame_preview() end,
-	['n <leader>glu'] = function() require('vgit').buffer_hunks_preview() end,
-	['n <leader>gls'] = function() require('vgit').project_hunks_staged_preview() end,
-	['n <leader>gd'] = function() require('vgit').project_diff_preview() end,
-	['n <leader>gq'] = function() require('vgit').project_hunks_qf() end,
-	['n <leader>gx'] = function() require('vgit').toggle_diff_preference() end,
-	['n <leader>gl'] = function() require('vgit').project_logs_preview() end,
-}
-
 return {
 	use_deps = function(use)
 		use {
-			'tanvirtin/vgit.nvim',
-			requires = {
-				'nvim-lua/plenary.nvim'
-			}
+			'akinsho/toggleterm.nvim',
 		}
 	end,
 
 	configure = function()
-		require('vgit').setup {
-			keymaps = keymaps,
-			settings = {
-				live_blame = {
-					enabled = false,
-				},
-			},
-		}
+
+		local Terminal = require('toggleterm.terminal').Terminal
+		local lazygit = Terminal:new({
+			cmd = 'lazygit --use-config-dir ~/.config/nvim/lua/basicIde/lazygitConfigDir || exit 100',
+			hidden = true,
+			direction = 'float',
+			close_on_exit = true,
+			on_exit = function (_, _, exit_code)
+				if exit_code == 100 then
+					vim.api.nvim_err_writeln('Please install lazygit executable. See https://github.com/jesseduffield/lazygit#installation')
+				end
+			end
+
+		})
+
+		vim.keymap.set('n', '<leader>g', function() lazygit:toggle() end, { desc = 'Open lazygit in terminal' })
+
 	end,
 }
