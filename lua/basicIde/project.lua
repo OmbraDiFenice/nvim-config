46792b9d995dc096700e5a1e11682fa91d6df766
Stefano <stefano.stoduto@gmail.com>
2023-02-12 13:00:42 +0100

    fix project custom script execution

--- lua/basicIde/project.lua
+++ lua/basicIde/project.lua
@@ -5,13 +5,7 @@ local function parseCommand(commandDescriptor)
 	local cmd = ""
 
 	if commandDescriptor.virtualEnv then
-		cmd = cmd .. "[[ -z ${VIRTUAL_ENV:+x} ]] || source " .. commandDescriptor.virtualEnv .. "; "
-	end
-
-	if commandDescriptor.env then
-		for key, value in pairs(commandDescriptor.env) do
-			cmd = cmd .. key .. "=" .. value .. " "
-		end
+		cmd = cmd .. "source " .. commandDescriptor.virtualEnv .. "; "
 	end
 
 	cmd = cmd .. commandDescriptor.cmd
@@ -22,11 +16,18 @@ end
 
 return {
 	use_deps = function(use)
-		use 'VonHeikemen/project-settings.nvim'
+		use {
+			'VonHeikemen/project-settings.nvim',
+			requires = {
+				'akinsho/toggleterm.nvim' -- not really required by the plugin, but used to start commands in terminal
+			}
+		}
 	end,
 
 	configure = function()
-		require('project-settings').setup({
+		local projectSettings = require('project-settings')
+
+		projectSettings.setup({
 			allow = {
 				unitTests = function(opts)
 					local cmd = parseCommand(opts)
@@ -48,11 +49,31 @@ return {
 						end
 
 						local cmd = parseCommand(commandDescriptor)
-						require('toggleterm').exec(cmd, CUSTOM_COMMAND_TERM_ID + i, 20, commandDescriptor.workDir)
+
+						local terminal = require('toggleterm.terminal').Terminal:new {
+							cmd = cmd,
+							dir = commandDescriptor.workDir,
+							env = commandDescriptor.env,
+							close_on_exit = false,
+						}
+
+						terminal:open(20)
 					end,
 					{ desc = 'Run custom script' })
 				end,
 			},
+		})
+
+		local project_cmds = vim.api.nvim_create_augroup('project_cmds', {clear = true})
+		local autocmd = vim.api.nvim_create_autocmd
+
+		autocmd('BufWritePost', {
+			pattern = '.vimrc.json',
+			group = project_cmds,
+			callback = function()
+				vim.cmd [[ silent ProjectSettingsRegister ]]
+				projectSettings.load({ force = true })
+			end
 		})
 	end,
 }
