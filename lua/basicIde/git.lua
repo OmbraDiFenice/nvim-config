142ca96fae928811415670da9f0d7eb48865761a
Stefano <stefano.stoduto@gmail.com>
2023-02-26 09:01:02 +0100

    switch to fugitive and gitsigns for git

--- lua/basicIde/git.lua
+++ lua/basicIde/git.lua
@@ -1,27 +1,48 @@
 return {
 	use_deps = function(use)
 		use {
-			'akinsho/toggleterm.nvim',
+			'tpope/vim-fugitive',
+		}
+		use {
+			'lewis6991/gitsigns.nvim',
 		}
 	end,
 
 	configure = function()
+		require('gitsigns').setup {
+		 on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
 
-		local Terminal = require('toggleterm.terminal').Terminal
-		local lazygit = Terminal:new({
-			cmd = 'lazygit --use-config-dir ~/.config/nvim/lua/basicIde/lazygitConfigDir || exit 100',
-			hidden = true,
-			direction = 'float',
-			close_on_exit = true,
-			on_exit = function (_, _, exit_code)
-				if exit_code == 100 then
-					vim.api.nvim_err_writeln('Please install lazygit executable. See https://github.com/jesseduffield/lazygit#installation')
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					opts.silent = true
+					vim.keymap.set(mode, l, r, opts)
 				end
+
+				-- Navigation
+				map('n', ']c', function()
+					if vim.wo.diff then return ']c' end
+					vim.schedule(function() gs.next_hunk() end)
+					return '<Ignore>'
+				end, {expr=true})
+
+				map('n', '[c', function()
+					if vim.wo.diff then return '[c' end
+					vim.schedule(function() gs.prev_hunk() end)
+					return '<Ignore>'
+				end, {expr=true})
+
+				-- Actions
+				map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+				map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+				map('n', '<leader>hp', gs.preview_hunk)
+				map('n', '<leader>hS', gs.stage_buffer)
+				map('n', '<leader>hu', gs.undo_stage_hunk)
+				map('n', '<leader>hR', gs.reset_buffer)
+				map('n', '<leader>hd', gs.diffthis)
+				map('n', '<leader>hD', function() gs.diffthis('~') end)
 			end
-
-		})
-
-		vim.keymap.set('n', '<leader>g', function() lazygit:toggle() end, { desc = 'Open lazygit in terminal' })
-
+		}
 	end,
 }
