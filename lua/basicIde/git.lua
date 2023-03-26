193609e53d34174b21f0d398a8c7af74ea33901f
Stefano <stefano.stoduto@gmail.com>
2023-03-26 09:27:57 +0200

    add description to git shortcuts

--- lua/basicIde/git.lua
+++ lua/basicIde/git.lua
@@ -34,14 +34,14 @@ return {
 				end, {expr=true})
 
 				-- Actions
-				map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-				map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-				map('n', '<leader>hp', gs.preview_hunk)
-				map('n', '<leader>hS', gs.stage_buffer)
-				map('n', '<leader>hu', gs.undo_stage_hunk)
-				map('n', '<leader>hR', gs.reset_buffer)
-				map('n', '<leader>hd', gs.diffthis)
-				map('n', '<leader>hD', function() gs.diffthis('~') end)
+				map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>', { desc = 'stage hunk' })
+				map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = 'reset hunk' })
+				map('n', '<leader>hp', gs.preview_hunk, { desc = 'git preview hunk' })
+				map('n', '<leader>hS', gs.stage_buffer, { desc = 'git stage buffer' })
+				map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
+				map('n', '<leader>hR', gs.reset_buffer, { desc = 'git reset buffer' })
+				map('n', '<leader>hd', gs.diffthis, { desc = 'git diff this' })
+				map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = 'git diff ~' })
 			end
 		}
 	end,
