e71b4e3a782f1523545c89605bf0e8ad060e38cd
Stefano <stefano.stoduto@gmail.com>
2023-07-09 07:50:09 +0200

    rewrite help window autocmd in lua and move it to editor component

--- lua/basicIde/editor.lua
+++ lua/basicIde/editor.lua
@@ -9,5 +9,18 @@ return {
 		require('auto-save').setup()
 
 		vim.keymap.set('n', '<leader>Q', function() vim.cmd('qall') end, { desc = 'close window' })
+
+		-- opens help windows on the right, taken from https://vi.stackexchange.com/questions/4452/how-can-i-make-vim-open-help-in-a-vertical-split
+		local vimrc_help_group = vim.api.nvim_create_augroup('vimrc_help', { clear = true })
+		vim.api.nvim_create_autocmd( { 'BufEnter' }, {
+			desc = 'Move opened help windows to the right',
+			group = vimrc_help_group,
+			pattern = '*.txt',
+			callback = function (args)
+				if vim.api.nvim_buf_get_option(args.buf, 'buftype') == 'help' then
+					vim.cmd 'wincmd L'
+				end
+			end
+		})
 	end
 }
--- lua/basicIde/vimSettings.lua
+++ lua/basicIde/vimSettings.lua
@@ -18,13 +18,5 @@ return {
 		vim.wo.signcolumn = 'yes'
 
 		vim.cmd [[ set clipboard+=unnamedplus ]]
-
-		-- opens help windows on the right, taken from https://vi.stackexchange.com/questions/4452/how-can-i-make-vim-open-help-in-a-vertical-split
-		vim.cmd [[
-			augroup vimrc_help
-				autocmd!
-				autocmd BufEnter *.txt if &buftype == 'help' | wincmd L | endif
-			augroup END
-		]]
 	end,
 }
