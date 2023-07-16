69d7009653c41ae2aa21ba5c0822695f335ad99c
Stefano <stefano.stoduto@gmail.com>
2023-07-16 08:34:29 +0200

    better handling of tree in session

--- lua/basicIde/session.lua
+++ lua/basicIde/session.lua
@@ -2,7 +2,7 @@ local close_nvim_tree = function()
 	local nvim_tree_api = require('nvim-tree.api')
 
 	if nvim_tree_api ~= nil then
-		nvim_tree_api.tree.close()
+		nvim_tree_api.tree.close_in_all_tabs()
 	end
 end
 
@@ -11,7 +11,10 @@ local open_nvim_tree = function()
 	local nvim_tree_view = require('nvim-tree.view')
 
 	if nvim_tree_api ~= nil and nvim_tree_view ~= nil then
-		nvim_tree_api.tree.open()
+		if not nvim_tree_view.is_visible() then
+			nvim_tree_api.tree.open()
+		end
+
 		if vim.api.nvim_get_current_buf() == nvim_tree_view.get_bufnr() and vim.fn.expand('#') ~= '' then
 			vim.cmd [[ :e # ]]
 		end
