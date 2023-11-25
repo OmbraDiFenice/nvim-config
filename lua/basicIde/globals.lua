472a201a3a50485ec119c99133e1def14809cf3b
Stefano <stefano.stoduto@gmail.com>
2023-11-26 00:22:00 +0100

    type hint globals and utils

--- lua/basicIde/globals.lua
+++ lua/basicIde/globals.lua
@@ -1,25 +1,45 @@
 local utils = require('basicIde.utils')
 
+---Print the given input as nvim message, including tables
+---@param table any # what to print. It also accepts tables, in which case prints the content recursively
+---@return any # returns the same input it was passed
 P = function(table)
 	vim.api.nvim_echo({ { vim.inspect(table) } }, true, {})
 	return table
 end
 
+---Prints the lines of the given array as a single nvim message
+---@param lines string[]
+---@return nil
 function Printlines(lines)
 	vim.notify(table.concat(lines, '\n'))
 end
 
--- https://stackoverflow.com/a/54775280/4046810
+---Checks if the given file path exists
+---taken from https://stackoverflow.com/a/54775280/4046810
+---@param name string # the path to the file to check
+---@return boolean
 File_exists = function(name)
 	local f = io.open(name, "r")
-	return f ~= nil and io.close(f)
+	local closed = io.close(f)
+	return f ~= nil and closed ~= nil and closed
 end
 
+---Returns the directory to be used to store data related to the current nvim session.
+---@return string # the full path to the folder holding session data
 Get_data_directory = function()
-	return utils.ensure_trailing_slash(vim.fn.stdpath("data")) .. "sessions/" .. utils.ensure_trailing_slash(utils.ensure_no_leading_slash(vim.fn.getcwd()))
+	local data_path = vim.fn.stdpath("data")
+	local cwd_path = vim.fn.getcwd()
+	---@cast data_path string
+	---@cast cwd_path string
+	return utils.ensure_trailing_slash(data_path) .. "sessions/" .. utils.ensure_trailing_slash(utils.ensure_no_leading_slash(cwd_path))
 end
 
--- taken from http://lua-users.org/wiki/CopyTable
+---Recursively copy the given value. Applies to tables too.
+---Taken from http://lua-users.org/wiki/CopyTable
+---@generic T: any
+---@param orig T # value to copy, can be a table
+---@return T # deep copy of the input value
 function Deepcopy(orig)
 	local orig_type = type(orig)
 	local copy
@@ -35,9 +55,14 @@ function Deepcopy(orig)
 	return copy
 end
 
--- taken from https://stackoverflow.com/a/7470789
--- Overwrites value of table t1 with corresponding values from t2.
--- Mutate and return t1
+---Overwrites value of table t1 with corresponding values from t2.
+---Mutate and return t1
+---taken from https://stackoverflow.com/a/7470789
+---@generic T: table
+---@generic U: table
+---@param t1 T # table to merge `t2` into. This will be mutated
+---@param t2 U # table to merge into `t1`. This will not be mutated
+---@return T | U # returns the mutated `t1`, containing values from the original `t1` overwritten/extended (recursively) with values from `t2`
 function Deepmerge(t1, t2)
 	for k, v in pairs(t2) do
 		if (type(v) == "table") and (type(t1[k] or false) == "table") then
@@ -49,20 +74,38 @@ function Deepmerge(t1, t2)
 	return t1
 end
 
+---Log the given message as warning in nvim messages.
+---The message is prefixed with an highlighted 'Warning: ' string
+---@param message string
+---@return nil
 function LogWarning(message)
 	vim.cmd('echohl WarningMsg')
 	vim.cmd('echomsg "Warning: ' .. message .. '"')
 	vim.cmd('echohl None')
 end
 
+---Checks if `needle` is contained in `haystack`
+---@generic T
+---@param needle T # the value to search for
+---@param haystack T[] # the array in which to search
+---@return boolean # true if `needle` is contained in `haystack` at least once
 function Is_in_list(needle, haystack)
 	for _, element in ipairs(haystack) do
 		if needle == element then
 			return true
 		end
 	end
+	return false
 end
 
+---Builds an array where each element is the result of applying `fn` to each value from `t`
+---@generic K: any
+---@generic V: any
+---@generic MappedV: any
+---@alias T table<K, V>
+---@param t T # input table on which to apply `fn`
+---@param fn fun(value: V, key: K, t: T): MappedV # invoked for each element of `t` and should compute the mapped value to insert in the resulting array. Receives the `value`, the `key` and a reference to the whole input `t` table. WARNING: do not mutate `t` inside `fn`
+---@return MappedV[] # a new array containing the mapped values
 function Map(t, fn)
 	local mapped = {}
 	for key, value in pairs(t) do
--- lua/basicIde/utils.lua
+++ lua/basicIde/utils.lua
@@ -1,14 +1,24 @@
 return {
+	---Ensures that the path string passed in has a trailing forward slash /
+	---@param path string
+	---@return string
 	ensure_trailing_slash = function(path)
 		if string.sub(path, #path, #path) == '/' then return path end
 		return path .. '/'
 	end,
 
+	---Ensures that the path string passed in has no leading forward slash /
+	---@param path string
+	---@return string
 	ensure_no_leading_slash = function(path)
 		if string.sub(path, 1, 1) == '/' then return string.sub(path, 2) end
 		return path
 	end,
 
+	---Open a simple floating window with the given options. The purpose is to give a way to open a float
+	---window that's consistent across the IDE by providing some defaults for the normal options from |vim.api.nvim_open_win()|.
+	---@param custom_options? table # same options passed to |vim.api.nvim_open_win()|. Defaults to a float with a single border and centered in the current window. If provided, those defaults are still used as a base, but you can override whatever you want
+	---@return integer # the bufnr of the buffer contained in the float
 	openFloatWindow = function(custom_options)
 		local win_width  = vim.api.nvim_win_get_width(0)
 		local win_height = vim.api.nvim_win_get_height(0)
@@ -30,9 +40,7 @@ return {
 			border = 'single',
 		}
 
-		if custom_options == nil then
-			custom_options = {}
-		end
+		if custom_options == nil then custom_options = {} end
 		options = Deepmerge(options, custom_options)
 
 		local buf = vim.api.nvim_create_buf(false, false)
@@ -41,9 +49,19 @@ return {
 		return buf
 	end,
 
-	-- calls callback passing the output of the command and the exit code as a list of lines. Merges stdout and stderr
+	---Runs `command` with |vim.fn.jobstart| and calls `callback` when the process ends with the output of the command and the job exit code as parameters.
+	---
+	---Merges stdout and stderr
+	---@param command string[] | string # command to pass to |vim.fn.jobstart|
+	---@param callback fun(output: string[], exit_code: integer): nil # the callback that will be invoked once the job terminates. The output of the job is passed as an array of lines
+	---@param options any # options for |vim.fn.jobstart|. They will extend a set of basic options required to capture the job output, but they will be overwritten if you provide your own
+	---@return integer # the job id of the spawned nvim job, as returned by |vim.fn.jobstart()|
 	runAndReturnOutput = function(command, callback, options)
+		---@type string[]
 		local output = {}
+
+		---@param dst string[]
+		---@param lines string[]
 		local function appendLines(dst, lines)
 			for _, line in ipairs(lines) do
 				table.insert(dst, line)
@@ -60,7 +78,7 @@ return {
 		}
 
 		if options == nil then options = {} end
-		local options = Deepmerge(options, default_options)
+		options = Deepmerge(options, default_options)
 
 		return vim.fn.jobstart(command, options)
 	end,
