3a5e41dcdd4be313fe092ef2be83aed5cf3b723b
Stefano <stefano.stoduto@gmail.com>
2023-11-26 19:08:06 +0100

    type hint loader

--- lua/basicIde/loader/project_venv.lua
+++ lua/basicIde/loader/project_venv.lua
@@ -1,3 +1,6 @@
+---Prefix `filename` with the base path provided as command line parameter
+---@param filename string
+---@return string
 local function prefix(filename)
 	local base_path = arg[1]
 	local path_sep = package.config:sub(1, 1)
@@ -8,6 +11,8 @@ local function prefix(filename)
 	return base_path .. filename
 end
 
+---Load the project file from the current directory
+---@return ProjectSettings? # return nil if there was an error loading the file
 local function load_proj_file()
 	local proj_config_file = prefix('.nvim.proj.lua')
 	local fh = io.open(proj_config_file)
@@ -17,6 +22,7 @@ local function load_proj_file()
 	return assert(loadstring(proj_config_str))()
 end
 
+---Load the project file and prints the `virtual_environment` field, so the shell script can source it
 local function main()
 	local proj_config = load_proj_file()
 	if proj_config ~= nil and proj_config.virtual_environment ~= nil then
