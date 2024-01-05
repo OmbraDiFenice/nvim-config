9878acac72b1bdae6cbffbebbff54293dffa0dff
Stefano Stoduto <stefano.stoduto@gmail.com>
2024-01-05 09:54:44 +0100

    enable custom initialization scripts from project file

--- lua/basicIde/utils.lua
+++ lua/basicIde/utils.lua
@@ -82,4 +82,23 @@ return {
 
 		return vim.fn.jobstart(command, options)
 	end,
+
+	---Return the highlighted text in visual mode
+	---
+	---taken from https://github.com/nvim-telescope/telescope.nvim/issues/1923 
+	---@return string
+	get_visual_selection = function ()
+		vim.cmd('noau normal! "vy"')
+		local text = vim.fn.getreg('v')
+		vim.fn.setreg('v', {})
+
+		if text == nil then return '' end
+
+		text = string.gsub(text --[[@as string]], "\n", "")
+		if #text > 0 then
+			return text
+		else
+			return ''
+		end
+	end
 }
