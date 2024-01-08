7c117f3bf9ccd206d31f46803b2aaa090079942c
Stefano Stoduto <stefano.stoduto@gmail.com>
2024-01-08 13:45:08 +0100

    don't load virtualenv if already enabled

--- lua/basicIde/loader/nvim_loader.sh
+++ lua/basicIde/loader/nvim_loader.sh
@@ -4,5 +4,5 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 VENVPATH=$(lua "$SCRIPTPATH/project_venv.lua" "$PWD")
 
-[[ ${VENVPATH:-} != "" ]] && source "$VENVPATH/bin/activate"
+[[ ${VENVPATH:-} != "" && -z "$VIRTUAL_ENV" ]] && source "$VENVPATH/bin/activate"
 nvim $@
