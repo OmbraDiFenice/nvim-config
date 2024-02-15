058c2e616fc5917c3c918e920cfadd19cc6bae0f
Stefano Stoduto <stefano.stoduto@gmail.com>
2024-02-15 09:34:58 +0100

    add command to synchronize current file

--- lua/basicIde/remote_sync/init.lua
+++ lua/basicIde/remote_sync/init.lua
@@ -1,11 +1,25 @@
 local rsync_manager = require('basicIde.remote_sync.rsync_manager')
 
+local function sync_current_file()
+	local path = vim.api.nvim_buf_get_name(0)
+	vim.api.nvim_exec_autocmds('User', {
+		group = 'BasicIde.RemoteSync',
+		pattern = 'SyncFile',
+		data = {
+			path = path,
+		},
+	})
+end
+
 ---@type IdeModule
 return {
 	use_deps = function(use)
 	end,
 
 	configure = function(project_settings)
+		-- the group needs to exist even if the feature is disabled so that other code can still subscribe to that group events without errors
+		local augroup = vim.api.nvim_create_augroup('BasicIde.RemoteSync', {})
+
 		if not project_settings.remote_sync.enabled then return end
 
 		local manager = rsync_manager:new(project_settings.remote_sync)
@@ -13,14 +27,13 @@ return {
 
 		if project_settings.remote_sync.sync_on_save then
 			vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+				group = augroup,
 				desc = 'Synchronize the saved buffer on the remote machine',
 				callback = function(args)
 					manager:synchronize_file(args.file)
 				end,
 			})
 		end
-
-		local augroup = vim.api.nvim_create_augroup('BasicIde.RemoteSync', {})
 
 		vim.api.nvim_create_autocmd('User', {
 			group = augroup,
@@ -39,5 +52,7 @@ return {
 				manager:synchronize_directory(args.data.path)
 			end
 		})
+
+		vim.keymap.set('n', '<leader>rs', sync_current_file, { desc = 'Send the current file to the remote machine' })
 	end
 }
