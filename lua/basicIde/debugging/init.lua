eed02421dfb4738dad8cb8d8d8300dea8e611acd
Stefano Stoduto <stefano.stoduto@gmail.com>
2024-07-18 14:54:45 +0200

    fix keymap_coverage setup on custom dap configs

--- lua/basicIde/debugging/init.lua
+++ lua/basicIde/debugging/init.lua
@@ -24,19 +24,20 @@ return {
 
 	configure = function(project_settings)
 		local setup_listeners = require('basicIde.debugging.events')
-		local setup_project_settings = require('basicIde.debugging.project_settings')
+		local project_settings_manager = require('basicIde.debugging.project_settings')
 		local setup_default_ui = require('basicIde.debugging.default_ui')
 
 		setup_default_ui()
 		setup_listeners()
 
+		project_settings_manager.apply_custom_dap_configurations(project_settings.debugging.dap_configurations)
+
 		for _, language_module in ipairs(language_modules) do
 			language_module.configure(project_settings)
 		end
 
-		-- this needs to be done after configuring all the language language modules
-		-- otherwise the default dap configurations created by the modules will not exist yet
-		-- and they need to be patched as well (e.g. to set/extend the environment variables)
-		setup_project_settings(project_settings)
+		-- this needs to be done after having created all the custom dap configurations
+		-- because they need to be patched as well (e.g. to set/extend the environment variables)
+		project_settings_manager.setup_project_settings(project_settings)
 	end
 }
