055ac43ffff204387dc21a4ae93b4e953e12d760
Stefano <stefano.stoduto@gmail.com>
2024-07-29 20:53:17 +0200

    move remote sync mapping function in dedicated utils file

---
+++ lua/basicIde/remote_sync/utils.lua
@@ -0,0 +1,36 @@
+local utils = require('basicIde.utils')
+
+---Convert a local absolute path to the pair [local relative path, remote absolute path]
+---@param mappings string[][] # the path mappings from |RemoteSyncSettings|
+---@param file_path string # local path to be mapped. If the path doesn't start with a path separator it's assumed to be a relative path, and it's prepended with the root path
+---@return string|nil, string|nil # local relative path, remote absolute path. Both nil if it wasn't possible to map the input
+local function map_file_path(mappings, file_path)
+	if file_path:sub(1, 1) ~= OS.sep then
+		file_path = table.concat({utils.ensure_no_trailing_slash(vim.fn.getcwd()), file_path}, OS.sep)
+	end
+
+	local selected_prefix = ''
+	local source_relative_path = nil
+	local destination_root_path = nil
+
+	for _, mapping in ipairs(mappings) do
+		local local_prefix = mapping[1]
+		local remote_prefix = mapping[2]
+
+		if file_path:find(local_prefix, 1, true) == 1 and #selected_prefix < #local_prefix then
+			source_relative_path = utils.ensure_no_leading_slash(string.sub(file_path, #local_prefix+1))
+			destination_root_path = remote_prefix
+			selected_prefix = local_prefix
+		end
+	end
+
+	if source_relative_path ~= nil then
+		source_relative_path = utils.ensure_no_leading_slash("./" .. source_relative_path)
+	end
+
+	return source_relative_path, destination_root_path
+end
+
+return {
+	map_file_path = map_file_path,
+}
