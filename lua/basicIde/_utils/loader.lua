80f9fffbee137615e34e333f4960a90a25171f01
Stefano <stefano.stoduto@gmail.com>
2025-04-05 08:02:14 +0200

    custom command to set window title

--- lua/basicIde/_utils/loader.lua
+++ lua/basicIde/_utils/loader.lua
@@ -2,10 +2,18 @@ local _proc = require('basicIde._utils.proc')
 
 local M = {}
 
+local last_title = ''
 ---set terminal window title if supported by the launching terminal emulator
 ---@param title string
 M.set_title = function(title)
 	_proc.run('$SET_TITLE_SCRIPT ' .. title)
+	last_title = title
+end
+
+---returns the last title set on the window
+---@return string
+M.get_title = function()
+	return last_title
 end
 
 return M
