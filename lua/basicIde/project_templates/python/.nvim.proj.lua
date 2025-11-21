26bb1f5600e40bac830c9470eaea156454a4cbd5
Stefano Stoduto <stefano.stoduto@gmail.com>
2025-11-21 16:13:01 +0100

    improve python template

--- lua/basicIde/project_templates/python/.nvim.proj.lua
+++ lua/basicIde/project_templates/python/.nvim.proj.lua
@@ -4,16 +4,29 @@ return {
 	project_languages = {"python"},
 	loader = {
 		virtual_environment = "venv",
+		environment = {
+			PYTHONPATH = "${ide:PROJECT_ROOT}/src:${env:PYTHONPATH}",
+		},
 	},
 	debugging = {
 		dap_configurations = {
 			python = {
 				{
-					keymap = "<F9>",
+					keymap = "<S-F9>",
 					type = "python",
 					name = "sample configuration",
 					request = "launch",
 					program = "main.py",
+					console = "integratedTerminal",
+					cwd = "${ide:PROJECT_ROOT}",
+				},
+				{
+					keymap = "<F9>",
+					type = "python",
+					name = "Unit tests",
+					request = "launch",
+					module = "unittest",
+					args = "discover -s ${ide:PROJECT_ROOT}/tests",
 					console = "integratedTerminal",
 					cwd = "${ide:PROJECT_ROOT}",
 				},
