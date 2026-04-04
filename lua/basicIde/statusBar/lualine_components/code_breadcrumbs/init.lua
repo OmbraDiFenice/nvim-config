d27b42e966d772a389658f8b196ece4f5efa6675
Stefano Stoduto <stefano.stoduto@gmail.com>
2026-04-04 17:24:16 +0200

    optimize lualine breadcrumbs component
    - debounce updates
    - skip redraw of statusline when not necessary

--- lua/basicIde/statusBar/lualine_components/code_breadcrumbs/init.lua
+++ lua/basicIde/statusBar/lualine_components/code_breadcrumbs/init.lua
@@ -1,7 +1,9 @@
 ---@class CodeBreadcrumbs
 ---@field msg string
+---@field _timer uv_timer_t?
 local M = {
 	msg = '',
+	_timer = nil,
 }
 
 ---@class LanguageBreadcrumbHandler
@@ -32,19 +34,36 @@ function M:new()
 	setmetatable(o, self)
 	self.__index = self
 
-	vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
+	vim.api.nvim_create_autocmd({ 'CursorMoved', 'BufEnter' }, {
 		desc = 'Update code breadcrumbs statusline info',
-		callback = function() self:update() end,
+		callback = function() o:schedule_update() end,
 	})
 
 	return o
 end
 
+---Throttle breadcrumb recomputation while moving around the buffer.
+---@return nil
+function M:schedule_update()
+	if self._timer ~= nil then
+		self._timer:stop()
+		self._timer:close()
+	end
+
+	self._timer = vim.uv.new_timer()
+	self._timer:start(75, 0, vim.schedule_wrap(function()
+		if self._timer ~= nil then
+			self._timer:stop()
+			self._timer:close()
+			self._timer = nil
+		end
+		self:update()
+	end))
+end
+
 ---Recompute the value of the `msg` field and trigger `lualine.refresh()`
 ---@return nil
 function M:update()
-	local lualine = require('lualine')
-
 	local lang = vim.bo.filetype
 	local parsers = vim.treesitter.language.get_filetypes(lang)
 	if #parsers == 0 then return end
@@ -58,8 +77,11 @@ function M:update()
 	local language_handler = language_handlers[file_type]
 	if language_handler == nil then return {} end
 
-	self.msg = find_breadcrumbs(tree_node, language_handler)
-	lualine.refresh()
+	local next_msg = find_breadcrumbs(tree_node, language_handler)
+	if next_msg == self.msg then return end
+
+	self.msg = next_msg
+	require('lualine').refresh()
 end
 
 return M
